f4704c726189aab25b660b3f610beaa22abada8c
Jesualdo Gutierrez <Jesualdog@gmail.com>
2022-05-07 06:18:13 -0500

    Mejorando las validaciones del formulario

--- validacion.js
+++ validacion.js
@@ -1,5 +1,6 @@
 function validarDatos(){
     window.event.preventDefault();
+
     if(document.formulario.nombre.value==""){
         alert("Campo Nombre Obligatorio")
         document.formulario.nombre.focus();
@@ -8,8 +9,9 @@ function validarDatos(){
         alert("Campo E-mail Obligatorio")
         document.formulario.email.focus();
     }
-    else if(document.formulario.email.value.indexOf("@")==-1 || document.formulario.email.value.indexOf(".")==-1 ){
-        alert("El E-mail es invalido")
+    else if(document.formulario.email.value.indexOf("@")==-1 || 
+        document.formulario.email.value.indexOf(".")==-1 ){
+        alert("El E-mail es invalido, sugerencia: xxxxx@xxxxx.xxx")
         document.formulario.email.focus();
     }
     else if(document.formulario.asunto.value==""){
@@ -20,7 +22,14 @@ function validarDatos(){
         alert("Campo Mensaje Obligatorio")
         document.formulario.mensaje.focus(); 
     }
+
+    else if(document.formulario.mensaje.value.length <= 500){
+        
+    }else{
+        alert("Mensaje demasiado extenso")
+        document.formulario.mensaje.focus(); 
+    }
     
 }
 
-document.querySelector("form").addEventListener("submit",validarDatos,); 
+document.querySelector("form").addEventListener("submit",validarDatos); 
